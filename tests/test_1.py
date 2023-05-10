3765f127766dc5f128ef3a7107a5a345707dea03
Natalia Belo <nataliabelo.nb@gmail.com>
2023-05-09 22:49:50 -0300

    assert error_msg

--- tests/test_1.py
+++ tests/test_1.py
@@ -7,8 +7,11 @@ driver = webdriver.Chrome()
 driver.get('https://www.saucedemo.com/')
 
 
-driver.find_element(By.ID, "user-name").send_keys("standard_user")
-driver.find_element(By.ID, "password").send_keys("secret_sauce")
+# driver.find_element(By.ID, "user-name").send_keys("standard_user")
+# driver.find_element(By.ID, "password").send_keys("secret_sauce")
 driver.find_element(By.ID, "login-button").click()
+assert driver.current_url == 'https://www.saucedemo.com/', "Página alterada!"
 
+error_msg = driver.find_element(By.CLASS_NAME, 'error-message-container').text
+assert error_msg == 'Epic sadface: Username is required', 'Mensagem de erro não encontrada'
 time.sleep(3)
