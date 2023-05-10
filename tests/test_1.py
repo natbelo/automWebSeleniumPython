c5ef9789523b1a3a28fdd9fdcb33c1f0b4dff7d2
Natalia Belo <nataliabelo.nb@gmail.com>
2023-05-09 23:09:23 -0300

    test login button

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,17 +1,26 @@
 import time
 
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get('https://www.saucedemo.com/')
+class Test1:
+    @pytest.fixture()
+    def setup(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get('https://www.saucedemo.com/')
+        yield
+        self.driver.quit()
 
 
-# driver.find_element(By.ID, "user-name").send_keys("standard_user")
-# driver.find_element(By.ID, "password").send_keys("secret_sauce")
-driver.find_element(By.ID, "login-button").click()
-assert driver.current_url == 'https://www.saucedemo.com/', "Página alterada!"
+    # driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    # driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    def test_click_login_btn(self,setup):
+        self.driver.find_element(By.ID, "login-button").click()
+        assert self.driver.current_url == 'https://www.saucedemo.com/', "Página alterada!"
 
-error_msg = driver.find_element(By.CLASS_NAME, 'error-message-container').text
-assert error_msg == 'Epic sadface: Username is required', 'Mensagem de erro não encontrada'
-time.sleep(3)
+        error_msg = self.driver.find_element(By.CLASS_NAME, 'error-message-container').text
+        assert error_msg == 'Epic sadface: Username is required', 'Mensagem de erro não encontrada'
+        time.sleep(3)
+
+
