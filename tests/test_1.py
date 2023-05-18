5a98649839a5a62948279effded460ed68d5e640
Natalia Belo <nataliabelo.nb@gmail.com>
2023-05-17 22:16:11 -0300

    implementando test_efetuar_login

--- tests/test_1.py
+++ tests/test_1.py
@@ -6,15 +6,13 @@ from selenium.webdriver.common.by import By
 
 class Test1:
     @pytest.fixture()
+
     def setup(self):
         self.driver = webdriver.Chrome()
         self.driver.get('https://www.saucedemo.com/')
         yield
         self.driver.quit()
 
-
-    # driver.find_element(By.ID, "user-name").send_keys("standard_user")
-    # driver.find_element(By.ID, "password").send_keys("secret_sauce")
     def test_click_login_btn(self,setup):
         self.driver.find_element(By.ID, "login-button").click()
         assert self.driver.current_url == 'https://www.saucedemo.com/', "Página alterada!"
@@ -22,5 +20,6 @@ class Test1:
         error_msg = self.driver.find_element(By.CLASS_NAME, 'error-message-container').text
         assert error_msg == 'Epic sadface: Username is required', 'Mensagem de erro não encontrada'
         time.sleep(3)
+
 
 
